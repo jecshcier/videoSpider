d2934f41780cf500adc65a2647affef488afa729
jecshcier <alsk123111@icloud.com>
2018-04-08 08:40:48 +0800

    update

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use('/vs', express.static('dist'));
+app.use('/video_spider/vs', express.static('dist'));
 
 app.use('/video_spider', indexRouter);
 app.use('/users', usersRouter);
