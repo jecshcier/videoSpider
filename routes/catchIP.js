6478ba63e94121b3c4598a56a465e2adc907fcb4
jecshcier <alsk123111@icloud.com>
2018-04-10 13:52:17 +0800

    更新ip地址池支持

---
+++ routes/catchIP.js
@@ -0,0 +1,100 @@
+const request = require('superagent'); // 引入SuperAgent
+require('superagent-proxy')(request);
+const cheerio = require('cheerio')
+
+const callbackModel = () => {
+	return {
+		flag: false,
+		message: '',
+		data: null
+	}
+}
+
+const cn_ip = (ipList) => {
+	let info = callbackModel()
+	return new Promise((resolve, reject) => {
+		if (ipList.length >= 20) {
+			info.message = "ip资源充足，不需要爬取"
+			reject(info)
+			return;
+		}
+		catchIP(ipList, 1, (ok, data) => {
+			if (ok) {
+				console.log("ip爬取完成 -> 共计" + data.length + '个IP')
+				info.flag = true
+				info.message = "ip爬取完成 -> 共计" + data.length + '个IP'
+				info.data = data
+				resolve(info)
+			} else {
+				info.message = data
+				reject(info)
+			}
+		})
+	})
+}
+
+function catchIP(ipList, page, callback) {
+	request // 发起请求
+		.get('http://free-proxy.cz/zh/proxylist/country/CN/all/uptime/all/' + page)
+		.end((err, respons) => {
+			if (err || !respons) {
+				console.log(err)
+				console.log(respons)
+				callback(false, '错误：' + err + '--响应：' + respons)
+				return;
+			}
+			let body = respons.text;
+			const $ = cheerio.load(body)
+			let currentIPList = []
+				// console.log($("#proxy_list > tbody > tr").length)
+			$("#proxy_list > tbody > tr").each(function(index, el) {
+				let ipTd = $(el).find("td").eq(0).children('script')
+				let ipPort = $(el).find("td").eq(1).children('.fport').html()
+					// console.log(ipTd)
+				if (ipTd.length) {
+					let ipProtocol = $(el).find("td").eq(2).children('small').html().toLowerCase()
+					if (ipProtocol === 'http' || ipProtocol === 'https') {
+						let ipUrl = ipTd.html().replace(/document\.write\(Base64\.decode\(\"/, '').replace(/\"\)\)/, '')
+						ipUrl = new Buffer(ipUrl, 'base64').toString()
+						currentIPList.push(ipProtocol + '://' + ipUrl + ':' + ipPort)
+					}
+					return true
+				}
+			});
+			// 获取可用ip
+			console.log(currentIPList)
+				// 过滤ip
+			for (let i = 0; i < currentIPList.length; i++) {
+				request // 发起请求
+					.get('http://ip.chinaz.com/getip.aspx')
+					.proxy(currentIPList[i])
+					.timeout({
+						response: 3000, // Wait 5 seconds for the server to start sending,
+						deadline: 60000, // but allow 1 minute for the file to finish loading.
+					})
+					.end((err, respons) => {
+						if (err || !respons) {
+							console.log("----------------------------------->")
+							console.log(currentIPList[i] + "的测试结果：")
+							console.log("------------无效-------------->")
+							console.log(err)
+							console.log(respons)
+						}
+						console.log("----------------------------------->")
+						console.log(currentIPList[i] + "的测试结果：")
+						console.log("------------通过-------------->")
+						ipList.push(currentIPList[i])
+					})
+			}
+
+			setTimeout(() => {
+				// ip不足时，继续爬取
+				page++;
+				ipList = Array.from(new Set(ipList));
+				(page > 5) ? callback(true, ipList): ((ipList.length < 20) ? catchIP(ipList, page, callback) : (callback(true, ipList) && console.log("okokokokokook")))
+			}, 5000)
+		})
+}
+
+
+module.exports = cn_ip
--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,10 @@ const cheerio = require('cheerio')
 const request = require('superagent'); // 引入SuperAgent
 require('superagent-proxy')(request);
 
+const cn_ip = require('./catchIP')
+
 // const gulpfile = require('../gulpfile')
+
 
 const callbackModel = () => {
 	return {
@@ -18,73 +21,95 @@ const callbackModel = () => {
 
 global.verifyKey = new Buffer(Math.random() * 99999999 + '').toString('base64')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-	res.render('index', {
-		title: '视频资源站',
-		staticUrl: '/video_spider/vs',
-		apiUrl: '/video_player',
-		verifyKey: global.verifyKey
+
+cn_ip([]).then((info) => {
+	let ipList = info.data
+		/* GET home page. */
+	router.get('/', function(req, res, next) {
+		res.render('index', {
+			title: '视频资源站',
+			staticUrl: '/video_spider/vs',
+			apiUrl: '/video_player',
+			verifyKey: global.verifyKey
+		});
 	});
-});
 
-router.post('/searchVideo', function(req, res, next) {
-	let videoName = req.body.videoName
-	let info = callbackModel()
-	if (!videoName) {
-		info.message = "视频名称不能为空"
-		res.send(info)
-		return false
-	} else {
-		request // 发起请求
-			.get('http://www.soku.com/search_video/q_' + encodeURI(videoName))
-			// .proxy("http://117.185.105.114:8060")
-			.end((err, respons) => {
-				if (err) {
-					info.message = err
-					console.log("错误")
-					res.send(info)
-				} else {
-					let body = respons.text;
-					console.log(body)
-					const $ = cheerio.load(body)
-					let videoList = []
-					let re = /http/
-					$(".s_dir").each(function(index, el) {
-						console.log('==================>')
-						console.log(index)
-						console.log('==================>')
-						console.log($(el).html())
-						if (!$(el).find(".base_name").length) {
-							return true;
-						}
-						let videoData = {
-							name: $(el).find(".base_name").html().replace(/<[^>]+>/g, ""),
-							list: [],
-							thumb: $(el).find(".s_target>img").length ? $(el).find(".s_target>img").attr('src') : null
-						}
-						$(el).find(".s_items").each(function(index, el) {
-							videoData.list[index] = []
-							$(el).find('ul li a').each(function(index2, el2) {
-								let vu = $(el2).attr('href')
-								videoData.list[index].push({
-									num: $(el2).find('span').html(),
-									url: new Buffer(re.test(vu) ? vu : ('http://' + vu.replace(/\/\//, ''))).toString('base64'),
-									verifyKey: verifyKey
-								})
+	router.post('/searchVideo', function(req, res, next) {
+		let videoName = req.body.videoName
+		let info = callbackModel()
+		if (!videoName) {
+			info.message = "视频名称不能为空"
+			res.send(info)
+			return false
+		} else {
+			request // 发起请求
+				.get('http://www.soku.com/search_video/q_' + encodeURI(videoName))
+				.proxy(ipList[0] ? ipList[0] : null)
+				.end((err, respons) => {
+					if (err) {
+						info.message = '服务器连接出错' + err
+						console.log("错误")
+						ipList.splice(0, 1)
+						res.send(info)
+					} else {
+						let body = respons.text;
+						console.log(body)
+						const $ = cheerio.load(body)
+						let videoList = []
+						let re = /http/
+						$(".s_dir").each(function(index, el) {
+							console.log('==================>')
+							console.log(index)
+							console.log('==================>')
+							console.log($(el).html())
+							if (!$(el).find(".base_name").length) {
+								return true;
+							}
+							let videoData = {
+								name: $(el).find(".base_name").html().replace(/<[^>]+>/g, ""),
+								list: [],
+								thumb: $(el).find(".s_target>img").length ? $(el).find(".s_target>img").attr('src') : null
+							}
+							$(el).find(".s_items").each(function(index, el) {
+								videoData.list[index] = []
+								$(el).find('ul li a').each(function(index2, el2) {
+									let vu = $(el2).attr('href')
+									videoData.list[index].push({
+										num: $(el2).find('span').html(),
+										url: new Buffer(re.test(vu) ? vu : ('http://' + vu.replace(/\/\//, ''))).toString('base64'),
+										verifyKey: verifyKey
+									})
+								});
 							});
+							videoList.push(videoData)
 						});
-						videoList.push(videoData)
-					});
-					info.flag = true
-					info.message = "获取成功"
-					info.data = videoList
-					res.send(info)
-				}
-			});
-	}
-});
+						info.flag = true
+						info.message = "获取成功"
+						info.data = videoList
+						res.send(info)
+					}
+				});
+		}
+	});
+	// 三小时更新一次ip池
+	setInterval(() => {
+		cn_ip(ipList).then((info) => {
+			ipList = info.data
+		}).catch((info) => {
+			console.log("==========================>")
+			console.log(info.message)
+			console.log("==========================>")
+		})
+	}, 1000 * 60 * 60 * 3)
 
+}).catch((info) => {
+	console.log("==========================>")
+	console.log(info.message)
+	console.log("==========================>")
+})
+
+
+// 24小时更新一次key
 setInterval(() => {
 	global.verifyKey = new Buffer(Math.random() * 99999999 + '').toString('base64')
 }, 1000 * 60 * 60 * 24)
